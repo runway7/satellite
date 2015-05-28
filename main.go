24c0cd34cacbb90e5efcb4a126c06e0e2b7ba77e
Arpith <arpith@feedreader.co>
2015-05-28 21:14:21 +0530

    max active

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 func newPool(server string) *redis.Pool {
 	return &redis.Pool{
 		MaxIdle:     3,
+		MaxActive: 120,
 		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
 			log.Println("Dialling redis - "+server)
