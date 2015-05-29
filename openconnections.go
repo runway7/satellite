86f15a608009d3a7de273c9fffe610923698b1b0
Arpith Siromoney <arpith@feedreader.co>
2015-05-29 15:15:06 +0530

    use closure instead

--- openconnections.go
+++ openconnections.go
@@ -7,17 +7,17 @@ import (
 	"github.com/runway7/satellite/Godeps/_workspace/src/github.com/garyburd/redigo/redis"
 )
 
-func (p *redis.Pool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		c := p.Get()
-		defer c.Close()
-		connectionCount := c.Do("GET", "connection-count")
-		
-		fmt.Fprintf(w, connectionCount)
-	}
-}
+
 
 func NewOpenConnectionsHandler(pool *redis.Pool) func(w http.ResponseWriter, req *http.Request) {
-	return pool.ServeHTTP
+	return func (w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			c := pool.Get()
+			defer c.Close()
+			connectionCount := c.Do("GET", "connection-count")
+		
+			fmt.Fprintf(w, connectionCount)
+		}
+	}
 }
