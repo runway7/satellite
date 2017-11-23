a3c54ab960b3beef1228d129fbbf2b43774c6043
Sudhir Jonathan <sudhir.j@gmail.com>
2017-11-24 01:38:02 +0530

    Removed redis and started switching to an s3 based system.

--- main.go
+++ main.go
@@ -3,83 +3,60 @@ package main
 import (
 	"net/http"
 	"os"
-	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/julienschmidt/httprouter"
+	"github.com/urfave/negroni"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/sns"
 	"github.com/aws/aws-sdk-go/service/sqs"
 
-	"github.com/garyburd/redigo/redis"
 	"github.com/rs/cors"
 )
 
 func main() {
-	redisURL := os.Getenv("REDIS_URL")
-	if redisURL == "" {
-		redisURL = "localhost:6379"
+
+	awsSession, err := session.NewSession()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	session := session.New(&aws.Config{
-		Region: aws.String(os.Getenv("AWS_REGION")),
-		Credentials: credentials.NewStaticCredentials(
-			os.Getenv("AWS_ACCESS_KEY_ID"),
-			os.Getenv("AWS_SECRET_ACCESS_KEY"),
-			"",
-		),
+	authorizer := Authorizer{
+		configBucket: os.Getenv("CONFIG_BUCKET"),
+		s3Client:     s3.New(awsSession),
+	}
+
+	satellite := NewSatellite(SatelliteConfig{
+		sqsClient:   sqs.New(awsSession),
+		snsClient:   sns.New(awsSession),
+		s3Client:    s3.New(awsSession),
+		s3Uploader:  s3manager.NewUploader(awsSession),
+		eventBucket: os.Getenv("EVENT_BUCKET"),
+		authorizer:  authorizer,
 	})
-	outbox := os.Getenv("OUTBOX_ARN")
-	inbox := os.Getenv("INBOX_QUEUE_URL")
-	configBucket := "satellite-config-" + os.Getenv("AWS_REGION")
-	redisPool := newPool(redisURL)
-	sqsClient := sqs.New(session)
-	snsClient := sns.New(session)
-	s3Client := s3.New(session)
 
-	satellite := NewSatellite(outbox, configBucket)
-	satellite.SetRedisPool(redisPool)
-	satellite.SetSQSClient(sqsClient)
-	satellite.SetSNSClient(snsClient)
-	satellite.SetS3Client(s3Client)
+	//go satellite.StartSQSListener(inbox)
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 
-	go satellite.StartRedisListener()
-	go satellite.StartSQSListener(inbox)
+	})
+	router.GET("/:realm/:topic", satellite.Listen)
+	router.POST("/:realm/:topic", satellite.Post)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "7288"
 	}
-	handler := cors.New(cors.Options{
+	corsMiddleware := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET"},
+		AllowedMethods:   []string{"GET", "POST"},
 		AllowCredentials: true,
-	}).Handler(satellite)
-
-	log.WithFields(log.Fields{
-		"event":  "sat.start",
-		"region": os.Getenv("AWS_REGION"),
-	}).Info()
-
-	log.Fatal(http.ListenAndServe(":"+port, handler))
-}
-
-func newPool(host string) *redis.Pool {
-	return &redis.Pool{
-		MaxIdle:     2,
-		MaxActive:   5,
-		IdleTimeout: 5 * time.Second,
-		Wait:        true,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", host)
-			if err != nil {
-				log.Println(err)
-				return nil, err
-			}
-			return c, err
-		},
-	}
+	})
+	n := negroni.Classic()
+	n.UseHandler(router)
+	n.Use(corsMiddleware)
+	log.Fatal(http.ListenAndServe(":"+port, n))
 }
--- satellite.go
+++ satellite.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
+	"crypto/subtle"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -12,102 +11,103 @@ import (
 	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/julienschmidt/httprouter"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/sns"
 	"github.com/aws/aws-sdk-go/service/sqs"
-	"github.com/garyburd/redigo/redis"
+
 	"github.com/manucorporat/sse"
 )
+
+type SatelliteConfig struct {
+	sqsClient   *sqs.SQS
+	snsClient   *sns.SNS
+	s3Client    *s3.S3
+	s3Uploader  *s3manager.Uploader
+	eventBucket string
+	authorizer  Authorizer
+}
 
 // Satellite is a broadcaster that provides a http.Handler
 type Satellite struct {
 	antennas     map[string]*Antenna
 	antennaMutex sync.RWMutex
-	redisPool    *redis.Pool
-	sqsClient    *sqs.SQS
-	snsClient    *sns.SNS
-	s3Client     *s3.S3
-	outbox       string
-	configBucket string
+	config       SatelliteConfig
 }
 
-type topic struct {
-	path           string
-	pathComponents []string
-	realmID        string
-	topicID        string
-}
-type realmConfig struct {
+type Config struct {
 	Password string `json:"password"`
 }
 
-func parseTopic(path string) topic {
-	path = strings.Trim(path, "/")
-	pathComponents := strings.Split(path, "/")
-	realmID := pathComponents[0]
-	topicID := strings.Join(pathComponents[1:], "/")
-	return topic{
-		path:           path,
-		pathComponents: pathComponents,
-		realmID:        realmID,
-		topicID:        topicID,
-	}
+type Authorizer struct {
+	s3Client     *s3.S3
+	configBucket string
 }
 
-func (s *Satellite) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	descriptor := parseTopic(r.URL.Path)
-	if r.Method == "GET" {
-		s.listen(descriptor, w, r)
+func (s *Authorizer) load(realm string) (config Config, err error) {
+	configResponse, err := s.s3Client.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(s.configBucket),
+		Key:    aws.String(realm),
+	})
+	if err != nil {
+		log.Println(err)
+		return
 	}
 
-	if r.Method == "POST" {
-		s.post(descriptor, w, r)
+	configBytes, err := ioutil.ReadAll(configResponse.Body)
+	defer configResponse.Body.Close()
+	if err != nil {
+		log.Println(err)
+		return
 	}
+
+	err = json.Unmarshal(configBytes, &config)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	return
 }
-func (s *Satellite) post(descriptor topic, w http.ResponseWriter, r *http.Request) {
-	config, err := s.loadRealmConfig(descriptor.realmID)
+
+func (s *Satellite) Post(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	config, err := s.config.authorizer.load(params.ByName("realm"))
 	if err != nil {
 		http.Error(w, "Realm not recognized", http.StatusNotFound)
 		return
 	}
-	if config.Password != r.FormValue("token") {
+	if subtle.ConstantTimeCompare([]byte(config.Password), []byte(params.ByName("token"))) == 1 {
 		http.Error(w, "Wrong token", http.StatusForbidden)
 		return
 	}
-
-	message, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Could not read message", http.StatusBadRequest)
-		return
+	eventID := params.ByName("event_id")
+	if eventID == "" {
+		eventID = strconv.FormatInt(time.Now().UnixNano(), 26)
 	}
-	defer r.Body.Close()
-
-	response, err := s.snsClient.Publish(&sns.PublishInput{
-		Subject:  aws.String(descriptor.path),
-		Message:  aws.String(string(message)),
-		TopicArn: aws.String(s.outbox),
+	s.config.s3Uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(s.config.eventBucket),
+		Key: aws.String(strings.Join([]string{
+			params.ByName("realm"),
+			params.ByName("topic"),
+			eventID,
+		}, "/")),
+		Body: r.Body,
 	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	log.WithFields(log.Fields{
-		"event": "sat.publish",
-		"realm": descriptor.realmID,
-		"topic": descriptor.topicID,
-		"id":    *response.MessageId,
-		"table": "publishes",
-	}).Info()
 }
-func (s *Satellite) listen(descriptor topic, w http.ResponseWriter, r *http.Request) {
+
+func (s *Satellite) Listen(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	path := strings.Join([]string{params.ByName("realm"), params.ByName("topic")}, "/")
 	s.antennaMutex.Lock()
-	antenna, ok := s.antennas[descriptor.path]
+	antenna, ok := s.antennas[path]
 	if !ok {
 		antenna = NewAntenna()
-		s.antennas[descriptor.path] = antenna
+		s.antennas[path] = antenna
 	}
 	s.antennaMutex.Unlock()
 
@@ -123,33 +123,6 @@ func (s *Satellite) listen(descriptor topic, w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	sessionID := make([]byte, 18)
-	rand.Read(sessionID)
-
-	log.WithFields(log.Fields{
-		"event":   "sat.connection.start",
-		"id":      base64.StdEncoding.EncodeToString(sessionID) + "/b",
-		"session": base64.StdEncoding.EncodeToString(sessionID),
-		"realm":   descriptor.realmID,
-		"topic":   descriptor.topicID,
-		"ip":      r.RemoteAddr,
-		"table":   "connections",
-	}).Info()
-
-	defer func() {
-		log.WithFields(log.Fields{
-			"event":   "sat.connection.stop",
-			"id":      base64.StdEncoding.EncodeToString(sessionID) + "/e",
-			"session": base64.StdEncoding.EncodeToString(sessionID),
-			"realm":   descriptor.realmID,
-			"topic":   descriptor.topicID,
-			"ip":      r.RemoteAddr,
-			"table":   "connections",
-		}).Info()
-	}()
-
 	killSwitch := time.After(10 * time.Minute)
 	beam := antenna.Add(w)
 	defer antenna.Remove(beam)
@@ -157,46 +130,14 @@ func (s *Satellite) listen(descriptor topic, w http.ResponseWriter, r *http.Requ
 		select {
 		case <-closer.CloseNotify():
 			return
-
 		case <-time.After(45 * time.Second):
-			currentTime := strconv.Itoa(int(time.Now().UnixNano()))
 			beam.Pulse(sse.Event{
 				Event: "ping",
-				Data:  currentTime,
 			})
 		case <-killSwitch:
 			return
 		}
 	}
-}
-
-func (s *Satellite) loadRealmConfig(realm string) (realmConfig, error) {
-	configResponse, err := s.s3Client.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String(s.configBucket),
-		Key:    aws.String(realm),
-	})
-
-	if err != nil {
-		log.Println(err)
-		return realmConfig{}, err
-	}
-
-	configBytes, err := ioutil.ReadAll(configResponse.Body)
-	defer configResponse.Body.Close()
-	if err != nil {
-		log.Println(err)
-		return realmConfig{}, err
-	}
-
-	config := realmConfig{}
-
-	err = json.Unmarshal(configBytes, &config)
-	if err != nil {
-		log.Println(err)
-		return realmConfig{}, err
-	}
-
-	return config, nil
 }
 
 // Publish sends the given message to all subscribers of the given topic
@@ -210,56 +151,6 @@ func (s *Satellite) Publish(topic, message string) {
 			Event: "message",
 			Data:  message,
 		})
-		topicParts := strings.Split(topic, "/")
-		id := make([]byte, 18)
-		rand.Read(id)
-		go log.WithFields(log.Fields{
-			"event": "sat.delivery",
-			"realm": topicParts[0],
-			"topic": strings.Join(topicParts[1:], "/"),
-			"count": antenna.Size(),
-			"table": "deliveries",
-			"id":    base64.StdEncoding.EncodeToString(id),
-		}).Info()
-	}
-}
-
-// SetRedisPool sets the redis pool
-func (s *Satellite) SetRedisPool(pool *redis.Pool) {
-	s.redisPool = pool
-}
-
-// SetSQSClient sets the SQS client for use as the listener
-func (s *Satellite) SetSQSClient(client *sqs.SQS) {
-	s.sqsClient = client
-}
-
-// SetS3Client sets the S3 client for use as the listener
-func (s *Satellite) SetS3Client(client *s3.S3) {
-	s.s3Client = client
-}
-
-// SetSNSClient sets the SNS client for use as the listener
-func (s *Satellite) SetSNSClient(client *sns.SNS) {
-	s.snsClient = client
-}
-
-// StartRedisListener subscribes to Redis events and broadcasts them to
-// connected clients
-func (s *Satellite) StartRedisListener() {
-	r := s.redisPool.Get()
-	defer r.Close()
-	psc := redis.PubSubConn{Conn: r}
-	psc.PSubscribe("*")
-	defer psc.Close()
-	for {
-		switch n := psc.Receive().(type) {
-		case redis.PMessage:
-			go s.Publish(n.Channel, string(n.Data))
-		case error:
-			log.Printf("error: %v\n", n)
-			return
-		}
 	}
 }
 
@@ -272,7 +163,7 @@ type snsMessage struct {
 func (s *Satellite) StartSQSListener(inbox string) {
 	for {
 		log.WithField("event", "sat.sqs.inbox.open").Info()
-		resp, err := s.sqsClient.ReceiveMessage(&sqs.ReceiveMessageInput{
+		resp, err := s.config.sqsClient.ReceiveMessage(&sqs.ReceiveMessageInput{
 			QueueUrl:            aws.String(inbox),
 			WaitTimeSeconds:     aws.Int64(20),
 			MaxNumberOfMessages: aws.Int64(10),
@@ -285,10 +176,7 @@ func (s *Satellite) StartSQSListener(inbox string) {
 			go func(msg *sqs.Message) {
 				msgStruct := snsMessage{}
 				json.Unmarshal([]byte(*msg.Body), &msgStruct)
-				conn := s.redisPool.Get()
-				conn.Do("PUBLISH", msgStruct.Channel, msgStruct.Data)
-				conn.Close()
-				s.sqsClient.DeleteMessage(&sqs.DeleteMessageInput{
+				s.config.sqsClient.DeleteMessage(&sqs.DeleteMessageInput{
 					QueueUrl:      aws.String(inbox),
 					ReceiptHandle: msg.ReceiptHandle,
 				})
@@ -299,10 +187,9 @@ func (s *Satellite) StartSQSListener(inbox string) {
 }
 
 // NewSatellite creates a new satellite that handles pub sub. It provides a http.Handler
-func NewSatellite(outbox, configBucket string) *Satellite {
+func NewSatellite(config SatelliteConfig) *Satellite {
 	return &Satellite{
-		antennas:     make(map[string]*Antenna),
-		outbox:       outbox,
-		configBucket: configBucket,
+		antennas: make(map[string]*Antenna),
+		config:   config,
 	}
 }
